112246ca6b9d8f2be0e086efed139d1ee1fec646
AkselSukub <63051222+AkselSukub@users.noreply.github.com>
2024-02-13 17:24:24 +0300

    Upprog

--- prog/collection_converter.py
+++ prog/collection_converter.py
@@ -11,3 +11,14 @@ def collection_converter(type):
         else:
             return tuple(numbers)
     return inner_func
+
+if __name__ == "__main__":
+    # Пример использования
+    convert_to_list = collection_converter('list')
+    result_list = convert_to_list("1 2 3 4 5")
+
+    convert_to_tuple = collection_converter('tuple')
+    result_tuple = convert_to_tuple("1 2 3 4 5")
+
+    print(result_list)
+    print(result_tuple)
